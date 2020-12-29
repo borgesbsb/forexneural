fe435e1ced3c39464d87c6dd8899632a41b0b2e9
Benjamin  da Silva Borges <borges.bnjamin@gmail.com>
2020-12-29 12:08:43 -0300

    fix: space

--- server.py
+++ server.py
@@ -16,6 +16,7 @@ def server(host = '127.0.0.1', port=8082):
     sock.listen(5) 
     client, address = sock.accept()
     #Carreando o modelo neural
+    
     model = tf.keras.models.load_model('LSTM')
     try:
         while True: 
@@ -31,7 +32,7 @@ def server(host = '127.0.0.1', port=8082):
         pass            
 
 def predicao(valores):
-    
+
 
 
 
