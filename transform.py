fb48b3160609716dc1384a7eb287a9895ef51ab0
borgesbsb <borges.bnjamin@gmail.com>
2021-06-26 10:28:41 -0400

    fix: scripts python

--- transform.py
+++ transform.py
@@ -1,3 +1,6 @@
+#Esse arquivo faz o tratamento dos dados em formato reconhecido pela rede
+
+
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 import pandas as pd
@@ -7,4 +10,23 @@ import sys
 
 parser = lambda x: pd.datetime.strptime(x, "%d.%m.%Y %H:%M:%S")
 df = pd.read_csv( 'arquivos_de_coleta/EURUSD_15m.csv', sep=';', header=0, parse_dates=['time'],date_parser=parser)
-print(df)
+#print(df)
+
+lines = []
+
+#print(df.iloc[0,1:])
+
+for i in range(len(df)-4):
+    lines_tmp = []
+    for columns_  in  range(0,6):
+        lines_tmp.append(df.iloc[i,columns_])
+    for columns_  in  range(1,6):
+        lines_tmp.append(df.iloc[i+1,columns_])
+    for columns_  in  range(1,6):
+        lines_tmp.append(df.iloc[i+2,columns_])
+    for columns_  in  range(1,6):
+        lines_tmp.append(df.iloc[i+3,columns_])
+    lines.append(lines_tmp)        
+        
+df = pd.DataFrame(lines)      
+df.to_csv('transform_collection.csv', header=False, index=False)
