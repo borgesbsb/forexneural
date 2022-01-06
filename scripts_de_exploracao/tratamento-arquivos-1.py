fe83b5cb045fa4bb30284f0e4072b18064b884c4
gibrother <6832282+gibrother@users.noreply.github.com>
2022-01-06 19:31:09 -0400

    hdakjsd

--- scripts_de_exploracao/tratamento-arquivos-1.py
+++ scripts_de_exploracao/tratamento-arquivos-1.py
@@ -6,8 +6,8 @@ ref_arquivo = open("../arquivos_de_coleta/"+input,"r")
 f = open("../arquivos_de_coleta/"+output,'w')
 for linha in ref_arquivo:
     valores = linha.split()
-    data =  datetime.datetime.strptime(valores[0],'%Y.%m.%d').strftime('%m.%d.%Y')
-    line_string = data+" "+str(valores[1]+","+str(valores[2])+","+str(valores[3]+","+str(valores[4]+","+str(valores[5]))))
+    data =  datetime.datetime.strptime(valores[0],'%Y.%m.%d').strftime('%d.%m.%Y')
+    line_string = data+" "+str(valores[1])+","+str(valores[2])+","+str(valores[3])+","+str(valores[4])+","+str(valores[5])+","+str(valores[6])
     f.write("\n"+line_string)
 
 f.close()
